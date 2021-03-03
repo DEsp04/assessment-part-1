8ccace0605f9abb16f82b107793c48f900b63d63
david espinal <despinal0425@gmail.com>
2021-03-03 17:54:18 -0500

    completed first question

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//Why does React utilize the virtual-dom?
+//1 Why does React utilize the virtual-dom?
+/*
+  React uses virtual-dom to render the JSX in the react component into the DOM. However, the DOM that react uses is not the same one you see on the browser. It's a virtual DOM that performs faster than the real DOM. Any time you add a content using JSX or styling JSX the virtual DOM will interpret these changes and immediately render it on the browser.
+ */
+
+ //
