9231623efb5f7ff7dd58e08c77b73ef25d11a095
david espinal <despinal0425@gmail.com>
2021-03-03 18:04:17 -0500

    completed second question

--- index.js
+++ index.js
@@ -3,4 +3,10 @@
   React uses virtual-dom to render the JSX in the react component into the DOM. However, the DOM that react uses is not the same one you see on the browser. It's a virtual DOM that performs faster than the real DOM. Any time you add a content using JSX or styling JSX the virtual DOM will interpret these changes and immediately render it on the browser.
  */
 
- //
+ //2
+ /*
+  a. Data flow from parent component into children component. For example, index.js in a src folder will be the parent component of App.js and any additional components in the component folder. One way to demonstrate this flow is adding an attribute to a JSX that happens to be an imported child component inside App.js. Well you can retrieve the value from that props by going to that specific child component and console logging the value from that props.
+
+  b. The data flow from parent component to child component because it helps programmers understand the framework, especially when working on an app project. Also, many of the programmer who uses React framework are familiar with JS and so with JS you have the same idea of data flow. Where you can used export and import function into the index file in order to get an unique output.
+
+ */
